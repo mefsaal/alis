0b3477c1021cc47942a41d46e037311cbf915761
mefsaal <mefsaal@gmail.com>
2021-05-09 03:07:47 -0300

    Fix Btrfs boot

--- alis.sh
+++ alis.sh
@@ -1164,7 +1164,7 @@ function bootloader() {
         CMDLINE_LINUX="cryptdevice=PARTUUID=$PARTUUID_ROOT:$LUKS_DEVICE_NAME$BOOTLOADER_ALLOW_DISCARDS"
     fi
     if [ "$FILE_SYSTEM_TYPE" == "btrfs" ]; then
-        CMDLINE_LINUX="$CMDLINE_LINUX rootflags=subvol=root"
+        CMDLINE_LINUX="$CMDLINE_LINUX"
     fi
     if [ "$KMS" == "true" ]; then
         case "$DISPLAY_DRIVER" in
