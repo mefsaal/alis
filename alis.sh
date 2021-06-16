8fc11a41d99746e848cb76ed9fbd4ce2ad135b5b
Gabriel Davila <56521591+mefsaal@users.noreply.github.com>
2021-06-15 22:23:40 -0300

    Snpsht

--- alis.sh
+++ alis.sh
@@ -602,7 +602,7 @@ function partition() {
         btrfs subvolume create /mnt/@
         btrfs subvolume create /mnt/@home
         btrfs subvolume create /mnt/@var_log
-        btrfs subvolume create /mnt/@.snapshots
+        btrfs subvolume create /mnt/@snapshots
         umount /mnt
 
         mount -o "subvol=@,$PARTITION_OPTIONS,compress=zstd" "$DEVICE_ROOT" /mnt
@@ -611,7 +611,7 @@ function partition() {
         mount -o "$PARTITION_OPTIONS_BOOT" "$PARTITION_BOOT" /mnt/boot
         mount -o "subvol=@home,$PARTITION_OPTIONS_ROOT,compress=zstd" "$DEVICE_ROOT" /mnt/home
         mount -o "subvol=@var_log,$PARTITION_OPTIONS_ROOT,compress=zstd" "$DEVICE_ROOT" /mnt/var/log
-        mount -o "subvol=@.snapshots,$PARTITION_OPTIONS_ROOT,compress=zstd" "$DEVICE_ROOT" /mnt/.snapshots
+        mount -o "subvol=@snapshots,$PARTITION_OPTIONS_ROOT,compress=zstd" "$DEVICE_ROOT" /mnt/.snapshots
     else
         mount -o "$PARTITION_OPTIONS_ROOT" "$DEVICE_ROOT" /mnt
 
